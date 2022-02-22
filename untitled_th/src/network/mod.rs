f52883e6aa65fe4df8cb428d3d8c8a0f632f4d03
euOnmyoji <1418780411@qq.com>
2022-02-22 21:31:06 +0800

    push works

--- untitled_th/src/network/mod.rs
+++ untitled_th/src/network/mod.rs
@@ -1,21 +1,19 @@
 //! Network module for game
 //! For general packet format:
-//! Contain PACKET_HEADER VERSION(be u16) TYPE(be u16)
-
+//! Contain PACKET_HEADER VERSION(be u16) TYPE(be u16)  
+//! Following is packet data.
 
 pub const PACKET_HEADER: [u8; 3] = *b"uth";
 pub const VERSION: u16 = 0;
 pub const HEADER_LEN: usize = PACKET_HEADER.len() + std::mem::size_of::<u16>() * 2;
 
 pub mod server;
-
-#[repr(u16)]
-pub enum PacketType {
+pub mod packets {
     /// and following u8 as ping increasing index
-    Ping = 0,
+    pub const PING: u16 = 0;
     /// and following u8 as pong index
-    Pong = 1,
+    pub const PONG: u16 = 1;
     /// For client to server the following u8 as index(zero for first) and left all is username if first.
     /// For server to client the following u8 as bool and remaining for message.
-    Connect = 2,
+    pub const CONNECT: u16 = 2;
 }
--- untitled_th/src/network/server.rs
+++ untitled_th/src/network/server.rs
@@ -1,10 +1,13 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::net::SocketAddr;
 use std::sync::Arc;
 
+use byteorder::{ReadBytesExt, BE};
 use tokio::net::UdpSocket;
 use tokio::sync::mpsc::{Receiver, Sender};
 use tokio::sync::RwLock;
+
+use super::packets;
 
 pub enum LogicPacket {}
 
@@ -35,7 +38,6 @@ pub struct GameServer {
     connected: RwLock<HashMap<SocketAddr, ConnectedClient>>,
 }
 
-
 impl GameServer {
     pub async fn new(port: u16) -> tokio::io::Result<Self> {
         log::info!("Binding server port: {}", port);
@@ -50,11 +52,15 @@ impl GameServer {
         })
     }
 
+    /// loop network and should be tokio runtime
     pub async fn loop_network(self: Arc<Self>) -> tokio::io::Result<()> {
         let mut buf = [0; 32 * 1024];
         loop {
             match self.socket.recv_from(&mut buf).await {
                 Ok((n, addr)) => {
+                    let _now = std::time::SystemTime::now();
+                    let _now_ins = std::time::Instant::now();
+
                     if n < crate::network::HEADER_LEN {
                         log::debug!("{:?} sent packet with bytes less than header len", addr);
                         continue;
@@ -65,13 +71,23 @@ impl GameServer {
                         log::debug!("{:?} sent packet with wrong header", addr);
                         continue;
                     }
+                    // we checked the length
+                    let version = reader.read_u16::<BE>().unwrap();
+                    let packet_type = reader.read_u16::<BE>().unwrap();
 
                     let data = Vec::from(&buf[crate::network::HEADER_LEN..n]);
-
                     let server = self.clone();
                     tokio::spawn(async move {
                         let connected = server.connected.read().await;
-                        if let Some(client) = connected.get(&addr) {} else {}
+                        if let Some(client) = connected.get(&addr) {
+                        } else {
+                            match packet_type {
+                                packets::CONNECT => {}
+                                _ => {
+                                    log::debug!("{:?} sent packet but not connected", addr);
+                                }
+                            }
+                        }
                     });
                 }
                 Err(e) => {
